da8254a609f3edffd1e5efc528dbc757d014c550
karis-tlg <td.kha.18@gmail.com>
2025-08-03 08:04:16 +0700

    1

--- xmake.lua
+++ xmake.lua
@@ -1,19 +1,21 @@
 add_rules("mode.debug", "mode.release")
 
-add_repositories("liteldev-repo https://github.com/LiteLDev/xmake-repo.git")
+add_repositories("levimc-repo https://github.com/LiteLDev/xmake-repo.git")
 
+-- add_requires("levilamina x.x.x") for a specific version
+-- add_requires("levilamina develop") to use develop version
+-- please note that you should add bdslibrary yourself if using dev version
 if is_config("target_type", "server") then
-    add_requires("levilamina 1.4.1", {configs = {target_type = "server"}})
+    add_requires("levilamina", {configs = {target_type = "server"}})
 else
-    add_requires("levilamina 1.0.0-rc.3", {configs = {target_type = "client"}})
+    add_requires("levilamina", {configs = {target_type = "client"}})
 end
+
 add_requires("levibuildscript")
-add_requires("exprtk 0.0.3")
 
 if not has_config("vs_runtime") then
     set_runtimes("MD")
 end
-
 
 option("target_type")
     set_default("server")
@@ -21,65 +23,23 @@ option("target_type")
     set_values("server", "client")
 option_end()
 
-option("test")
-    set_default(false)
-    set_showmenu(true)
-option_end()
-
-rule("gen_version")
-    before_build(function(target)
-        import("scripts.gen_version")()
-    end)
-
-target("DynamicLight")
-    add_rules("gen_version")
+target("DynamicLight") -- Change this to your mod name.
     add_rules("@levibuildscript/linkrule")
     add_rules("@levibuildscript/modpacker")
-    add_cxflags(
-        "/EHa",
-        "/utf-8",
-        "/W4",
-        "/w44265",
-        "/w44289",
-        "/w44296",
-        "/w45263",
-        "/w44738",
-        "/w45204"
-    )
-    set_kind("shared")
-    set_symbols("debug")
-    set_languages("c++20")
+    add_cxflags( "/EHa", "/utf-8", "/W4", "/w44265", "/w44289", "/w44296", "/w45263", "/w44738", "/w45204")
+    add_defines("NOMINMAX", "UNICODE")
+    add_packages("levilamina")
     set_exceptions("none") -- To avoid conflicts with /EHa.
-    add_includedirs("src")
+    set_kind("shared")
+    set_languages("c++20")
+    set_symbols("debug")
     add_headerfiles("src/**.h")
-    add_files("src/**.cc")
-    add_defines(
-        "NOMINMAX",
-        "UNICODE",
-        "_HAS_CXX23=1",
-        "TPS_EXPORTS"
-    )
-    add_packages(
-        "levilamina",
-        "exprtk"
-    )
-
-    if is_mode("debug") then
-        add_defines("TPS_DEBUG"--[[ , "LL_I18N_COLLECT_STRINGS" ]])
-    end
-
-    if has_config("test") then
-        add_defines("TPS_TEST")
-        add_includedirs("test")
-        add_files("test/**.cc")
-    end
-
-    add_defines("MOD_NAME=\"DynamicLight\"")
-
-    after_build(function (target)
-        -- local bindir = path.join(os.projectdir(), "bin")
-        -- local outputdir = path.join(bindir, target:name())
-        -- -- copy data files
-        -- local datadir = path.join(os.projectdir(), "assets", "data")
-        -- os.cp(datadir, outputdir)
-    end)
+    add_files("src/**.cpp")
+    add_includedirs("src")
+    -- if is_config("target_type", "server") then
+    --     add_includedirs("src-server")
+    --     add_files("src-server/**.cpp")
+    -- else
+    --     add_includedirs("src-client")
+    --     add_files("src-client/**.cpp")
+    -- end
